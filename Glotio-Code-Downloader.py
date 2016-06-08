7d3d1c51886ec9e4b4b64dd764a44a75d5dbaba1
Wolfterro <wolfterrorista@hotmail.com>
2016-06-07 23:48:09 -0300

    Version 1.2 is now on GitHub! Check the CHANGELOG.txt for more details!

--- Glotio-Code-Downloader.py
+++ Glotio-Code-Downloader.py
@@ -3,37 +3,13 @@
 
 from __future__ import print_function
 from urllib2 import urlparse
-from bs4 import BeautifulSoup
 
 import os
 import sys
+import json
 import urllib2
 
-'''
-* The MIT License (MIT)
-* 
-* Copyright (c) 2016 Wolfgang Almeida <wolfgang.almeida@yahoo.com>
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-'''
-
-version = "1.1" 
+version = "1.2"
 
 # Checking, Creating and Accessing the Main Directory
 # ===================================================
@@ -47,69 +23,47 @@ def main_directory():
 
 # Checking, Creating and Accessing the Code's Directory
 # =====================================================
-def code_directory(title_dir):
-	if os.path.exists(title_dir):
-		os.chdir(title_dir)
+def code_directory(snippet_dir, get_id):
+	if os.path.exists(snippet_dir + "-[" + get_id + "]"):
+		os.chdir(snippet_dir + "-[" + get_id + "]")
 	else:
-		print("\n[Glot.io Code Downloader] Creating directory '" + title_dir + "' ...")
-		os.makedirs(title_dir)
-		os.chdir(title_dir)
+		print("[Glot.io Code Downloader] Creating directory '" + snippet_dir + "-[" + get_id + "]" + "' ...")
+		os.makedirs(snippet_dir + "-[" + get_id + "]")
+		os.chdir(snippet_dir + "-[" + get_id + "]")
 
-# Downloading the Code
-# ====================
-def get_codes(url, filenames):
-	soup = BeautifulSoup(response, 'html.parser')
+# Creating the Code Files
+# =======================
+def get_snippet_codes(num_codes):
+	for x in range(0, num_codes):
+		get_filename = config["files"][x]["name"].replace("/", "-")
+		
+		print("[Glot.io Code Downloader] Creating file '" + get_filename + "' ...", end="")
+		file_code = open(get_filename, "wb")
+		file_code.write(config["files"][x]["content"].encode('utf-8'))
+		file_code.close()
+		print(" OK!")
 
-	get_first_code = soup.findAll('div', {'class' : 'editor'}, id='editor-1')
-	get_second_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-2') 
-	get_third_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-3')
-	get_fourth_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-4')
-	get_fifth_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-5')
-	get_sixth_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-6')
-	codes_list = [get_first_code, get_second_code, get_third_code, get_fourth_code, get_fifth_code, get_sixth_code]
-
-	index_number = 0
-
-	for code in codes_list:
-		print("[Glot.io Code Downloader] Downloading code '" + filenames[index_number] + "' ...", end="")
-		file_code = open(filenames[index_number], "wb")
-		try:
-			file_code.write(code[0].contents[0].strip().encode('utf-8'))
-			print(" OK!")
-			file_code.close()
-			index_number += 1
-		except IndexError:
-			print(" No code! Skipping ...")
-			file_code.close()
-			os.remove(filenames[index_number])
-			index_number += 1
-
-# Recovering Some Informations and Returning Them
-# =============================================== 
-def code_information(url):
-	global response
-
+# Getting Information About the Snippet From it's JSON Data
+# =========================================================
+def get_snippet_json(url, api_url):
+	global config
+	
 	try:
-		response = urllib2.urlopen(url).read().decode('utf-8')
+		response = urllib2.urlopen(api_url).read().decode('utf-8')
 	except Exception:
-		print("\n[Glot.io Code Downloader] Error! Could not open URL '" + url + "'!")
+		print("[Glot.io Code Downloader] Error! Could not open URL '" + url + "'!")
 		print("[Glot.io Code Downloader] Check the URL or Internet connection and try again!")
 		sys.exit(1)
 
-	soup = BeautifulSoup(response, 'html.parser')
-	
-	get_title = soup.find('h2', id='snippet-title')
-	get_title_dir = get_title.text.replace("/", "-")
+	config = json.loads(response)
 
-	get_filenames = soup.findAll('span', {'class' : 'filename'})
-	filename_list = []
-	
-	for filename in get_filenames:
-		filename_list.append(filename.contents[0].strip().replace("/", "-"))
+	get_title = config["title"].encode('utf-8')
+	num_codes = len(config["files"])
+	snippet_dir = get_title.replace("/", "-")
 
-	return [get_title, get_title_dir, filename_list]
+	return [get_title, num_codes, snippet_dir]
 
-# Main Method 
+# Main Method
 # ===========
 def main():
 	main_directory()
@@ -117,17 +71,22 @@ def main():
 	print("=====================================")
 	print("Glot.io Code Downloader - Version %s" % (version))
 	print("=====================================\n")
-
+ 
 	url = raw_input("Insert the Snippet's URL: ")
-
 	url_split = urlparse.urlsplit(url)
-	if str(url_split[1]) != "glot.io":
-		print("\n[Glot.io Code Downloader] Error! Wrong website! Check the URL and try again!")
+
+	print(" ")
+	if str(url_split[1]) != "glot.io" and str(url_split[1]) != "snippets.glot.io":
+		print("[Glot.io Code Downloader] Error! Wrong website! Check the URL and try again!")
 		sys.exit(1)
 	else:
-		title, title_dir, filenames = code_information(url)
-		code_directory(title_dir)
-		get_codes(url, filenames)
+		get_id = os.path.basename(url)
+		api_url = "https://snippets.glot.io/snippets/" + get_id
+
+		get_title, num_codes, snippet_dir = get_snippet_json(url, api_url)
+
+		code_directory(snippet_dir, get_id)
+		get_snippet_codes(num_codes)
 
 # Initializing Program
 # ====================
---
+++ old/Glotio-Code-Downloader-1.1.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from urllib2 import urlparse
+from bs4 import BeautifulSoup
+
+import os
+import sys
+import urllib2
+
+'''
+* The MIT License (MIT)
+* 
+* Copyright (c) 2016 Wolfgang Almeida <wolfgang.almeida@yahoo.com>
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+'''
+
+version = "1.1" 
+
+# Checking, Creating and Accessing the Main Directory
+# ===================================================
+def main_directory():
+	if os.path.exists("Codes"):
+		os.chdir("Codes")
+	else:
+		print("[Glot.io Code Downloader] Creating directory 'Codes' ...")
+		os.makedirs("Codes")
+		os.chdir("Codes")
+
+# Checking, Creating and Accessing the Code's Directory
+# =====================================================
+def code_directory(title_dir):
+	if os.path.exists(title_dir):
+		os.chdir(title_dir)
+	else:
+		print("\n[Glot.io Code Downloader] Creating directory '" + title_dir + "' ...")
+		os.makedirs(title_dir)
+		os.chdir(title_dir)
+
+# Downloading the Code
+# ====================
+def get_codes(url, filenames):
+	soup = BeautifulSoup(response, 'html.parser')
+
+	get_first_code = soup.findAll('div', {'class' : 'editor'}, id='editor-1')
+	get_second_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-2') 
+	get_third_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-3')
+	get_fourth_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-4')
+	get_fifth_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-5')
+	get_sixth_code = soup.findAll('div', {'class' : 'editor hide'}, id='editor-6')
+	codes_list = [get_first_code, get_second_code, get_third_code, get_fourth_code, get_fifth_code, get_sixth_code]
+
+	index_number = 0
+
+	for code in codes_list:
+		print("[Glot.io Code Downloader] Downloading code '" + filenames[index_number] + "' ...", end="")
+		file_code = open(filenames[index_number], "wb")
+		try:
+			file_code.write(code[0].contents[0].strip().encode('utf-8'))
+			print(" OK!")
+			file_code.close()
+			index_number += 1
+		except IndexError:
+			print(" No code! Skipping ...")
+			file_code.close()
+			os.remove(filenames[index_number])
+			index_number += 1
+
+# Recovering Some Informations and Returning Them
+# =============================================== 
+def code_information(url):
+	global response
+
+	try:
+		response = urllib2.urlopen(url).read().decode('utf-8')
+	except Exception:
+		print("\n[Glot.io Code Downloader] Error! Could not open URL '" + url + "'!")
+		print("[Glot.io Code Downloader] Check the URL or Internet connection and try again!")
+		sys.exit(1)
+
+	soup = BeautifulSoup(response, 'html.parser')
+	
+	get_title = soup.find('h2', id='snippet-title')
+	get_title_dir = get_title.text.replace("/", "-")
+
+	get_filenames = soup.findAll('span', {'class' : 'filename'})
+	filename_list = []
+	
+	for filename in get_filenames:
+		filename_list.append(filename.contents[0].strip().replace("/", "-"))
+
+	return [get_title, get_title_dir, filename_list]
+
+# Main Method 
+# ===========
+def main():
+	main_directory()
+
+	print("=====================================")
+	print("Glot.io Code Downloader - Version %s" % (version))
+	print("=====================================\n")
+
+	url = raw_input("Insert the Snippet's URL: ")
+
+	url_split = urlparse.urlsplit(url)
+	if str(url_split[1]) != "glot.io":
+		print("\n[Glot.io Code Downloader] Error! Wrong website! Check the URL and try again!")
+		sys.exit(1)
+	else:
+		title, title_dir, filenames = code_information(url)
+		code_directory(title_dir)
+		get_codes(url, filenames)
+
+# Initializing Program
+# ====================
+if __name__ == "__main__":
+	main()
